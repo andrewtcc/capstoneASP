3c5ece1798df9e3ada12bd9e9bf8d5ef79578783
Andrew <andrew@example.com>
2020-12-15 11:46:09 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace capstoneASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
