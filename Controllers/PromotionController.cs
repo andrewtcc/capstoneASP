647e3860dc0d01e5641020ed15f520e158d64a20
Andrew <andrew@example.com>
2020-12-15 12:17:49 -0600

    Converted html to cshtml

---
+++ Controllers/PromotionController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using capstoneASP.Models;
+
+namespace capstoneASP.Controllers
+{
+    public class PromotionController : Controller
+    {
+        private readonly ILogger<PromotionController> _logger;
+
+        public PromotionController(ILogger<PromotionController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
